b948173f9c429b8b4271664397dd39ec27965c84
Alessandro Aronica <ale.aronica@outlook.it>
2025-12-11 23:20:12 +0100

    fixed import error in FirstPage.jsx

--- buildweek4/src/components/FirstPage/FirstPage.jsx
+++ buildweek4/src/components/FirstPage/FirstPage.jsx
@@ -1,7 +1,7 @@
 import "./style.css";
 import { Container, Row, Col } from "react-bootstrap";
 import LefColAccount from "../LeftColAccount/LeftColAccout";
-import ReducedCustomFooter from "../customFooters/reducedCustomFooter/ReducedCustomFooter
+import ReducedCustomFooter from "../customFooters/reducedCustomFooter/ReducedCustomFooter"
 import CustomNews from "../customNews/CustomNews";
 import Advertise from "../advertise/Advertise";
 import MessageBox from "../messages/MessageBox";
