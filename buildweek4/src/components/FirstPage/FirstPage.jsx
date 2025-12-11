102949193de8c61d27ced25c21600b90ceec4361
sara zanotto <saraazanotto@gmail.com>
2025-12-11 16:22:43 +0100

    inizio parte centrale

--- buildweek4/src/components/FirstPage/FirstPage.jsx
+++ buildweek4/src/components/FirstPage/FirstPage.jsx
@@ -1,21 +1,22 @@
 import "./style.css";
 import { Container, Row, Col } from "react-bootstrap";
 import LefColAccount from "../LeftColAccount/LeftColAccout";
+import MainPage from "../mainPage/MainPage";
 const FirstPage = () => {
   return (
     <>
       <Container>
-        <Row>
+        <Row className="g-5">
           <Col
-            md={3}
+            md={3} lg={3}
             className="d-flex align-items-center flex-column p-0 bg-dark"
           >
             <LefColAccount />
           </Col>
-          <Col md={5} className="bg-warning  ">
-            Prova
+          <Col md={9} lg={6} className="  ">
+           <MainPage/>
           </Col>
-          <Col md={3} className="bg-danger">
+          <Col lg={3} className="bg-danger d-none d-lg-block">
             Col
           </Col>
         </Row>
---
+++ buildweek4/src/components/mainPage/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SectionSearchInput from "./sectionSearch/SectionSearchInput";
+
+import SectionOrderPost from "./sectionOrderPost/SectionOrderPost";
+
+const MainPage = () => {
+  return (
+    <div>
+      <SectionSearchInput />
+     <SectionOrderPost/>
+    </div>
+  );
+};
+
+export default MainPage;
