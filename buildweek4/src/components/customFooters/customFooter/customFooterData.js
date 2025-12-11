520413dbd58b34bdf9d4a23371c8f7def3ae075a
Alessandro Aronica <ale.aronica@outlook.it>
2025-12-11 12:35:13 +0100

    uploaded footers

---
+++ buildweek4/src/components/customFooters/customFooter/customFooterData.js
@@ -0,0 +1,80 @@
+export const customFooterLinks = [
+  {
+    label: 'Informazioni',
+    href: '#'
+  },
+  {
+    label: 'Accessibilità',
+    href: '#'
+  },
+  {
+    label: 'Talent Solutions',
+    href: '#'
+  },
+  {
+    label: 'Informativa sulla community professionale',
+    href: '#'
+  },
+  {
+    label: 'Carriera',
+    href: '#'
+  },
+  {
+    label: 'Soluzioni di marketing',
+    href: '#'
+  },
+  {
+    label: 'Privacy e condizioni',
+    href: '#',
+    submenu: [
+      {
+        label: 'Informativa sulla privacy',
+        href: '#'
+      },
+      {
+        label: 'Contratto di licenza',
+        href: '#'
+      },
+      {
+        label: 'Termini e condizioni delle pagine',
+        href: '#'
+      },
+      {
+        label: 'Informativa sui cookie',
+        href: '#'
+      },
+      {
+        label: 'Informativa sul copyright',
+        href: '#'
+      },
+      {
+        label: "Opzioni relative all'informativa sulla privacy (Stato della California)",
+        href: '#'
+      }
+    ]
+  },
+  {
+    label: 'Opzioni per gli annunci pubblicitari',
+    href: '#'
+  },
+  {
+    label: 'Pubblicità',
+    href: '#'
+  },
+  {
+    label: 'Sales Solutions',
+    href: '#'
+  },
+  {
+    label: 'Mobile',
+    href: '#'
+  },
+  {
+    label: 'Piccole imprese',
+    href: '#'
+  },
+  {
+    label: 'Centro sicurezza',
+    href: '#'
+  }
+]
