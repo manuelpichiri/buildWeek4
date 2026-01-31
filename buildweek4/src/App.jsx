3e791aba00e7d62c6c04dc745729cd4d924240c7
Alessandro Aronica <ale.aronica@outlook.it>
2026-01-31 22:38:08 +0100

    updated frontend: added loginPage and registrationPage

--- buildweek4/src/App.jsx
+++ buildweek4/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import FirstPage from "./components/FirstPage/FirstPage";
-import NavBarLinkedin from "./components/customNavbar/NavbarLinkedin";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
+import LoginPage from "./components/loginPage/LoginPage";
+import RegistrationPage from "./components/registrationPage/RegistrationPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/registration" element={<RegistrationPage />} />
           <Route index path="/" element={<FirstPage />} />
           <Route path="/PaginaProfilo" element={<ProfilePage />} />
           <Route path="*" element={<ErrorPage />} />
